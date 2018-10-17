b6046919fc1d479141a2588733ae57266ac7af99
OpenNOX <ckaliszewski@gmail.com>
2018-10-16 21:45:01 -0400

    Update indent spacing

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 (function () {
 
-    var $result = document.getElementById("result");
+  var $result = document.getElementById("result");
 
-    function update () {
-        var result = null
-          , input = editor.getValue()
-          ;
+  function update() {
+    var result = null
+      , input = editor.getValue()
+      ;
 
-        try {
-            result = ejs.render(input)
-            $result.parentNode.style.background = "#27ae60";
-        } catch (e) {
-            result = e.stack;
-            $result.parentNode.style.background = "#c0392b";
-        }
-
-        $result.textContent = result;
+    try {
+      result = ejs.render(input)
+      $result.parentNode.style.background = "#27ae60";
+    } catch (e) {
+      result = e.stack;
+      $result.parentNode.style.background = "#c0392b";
     }
 
-    var editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/ejs");
-    editor.on("change", update);
-    editor.setValue(`<%
+    $result.textContent = result;
+  }
+
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/ejs");
+  editor.on("change", update);
+  editor.setValue(`<%
 const data = [{ ID: 1, MESSAGE: 'Hello, World!' },{ ID: 2, MESSAGE: 'Foo Bar' },{ ID: 3 }];
 -%>
 <%
@@ -39,5 +39,5 @@ JSON.stringify(recordObj);
 <%
 });
 %>`, -1);
-    editor.focus();
+  editor.focus();
 })();

cb78501d9e761f5e1e6f902e59eb5c073f98a19b
OpenNOX <ckaliszewski@gmail.com>
2018-10-16 22:36:56 -0400

    Add persistence between sessions (only if render successful)

--- js/main.js
+++ js/main.js
@@ -1,43 +1,46 @@
 (function () {
+  const defaultValue = `<%\nconst data = [{ ID: 1, MESSAGE: 'Hello, World!' },{ ID: 2, MESSAGE: 'Foo Bar' },{ ID: 3 }];\n-%>\n<%\ndata.forEach((record) => {\n  const recordObj = {\n    ...record\n  };\n\n  recordObj.hasMessage = !_.isNil(record.MESSAGE);\n%>\n<%-\nJSON.stringify(recordObj);\n-%>\n<%\n});\n%>`;
+  const resultEl = document.getElementById('result');
 
-  var $result = document.getElementById("result");
+  const editor = ace.edit('editor');
+  editor.setTheme('ace/theme/monokai');
+  editor.getSession().setMode('ace/mode/ejs');
+  editor.setValue(window.localStorage.getItem('previous-session') || defaultValue, -1);
+  editor.focus();
 
-  function update() {
-    var result = null
-      , input = editor.getValue()
-      ;
-
-    try {
-      result = ejs.render(input)
-      $result.parentNode.style.background = "#27ae60";
-    } catch (e) {
-      result = e.stack;
-      $result.parentNode.style.background = "#c0392b";
-    }
-
-    $result.textContent = result;
-  }
-
-  var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/ejs");
-  editor.on("change", update);
-  editor.setValue(`<%
-const data = [{ ID: 1, MESSAGE: 'Hello, World!' },{ ID: 2, MESSAGE: 'Foo Bar' },{ ID: 3 }];
--%>
-<%
-data.forEach((record) => {
-  const recordObj = {
-    ...record
+  const colors = {
+    success: 'rgb(39, 174, 96)',
+    failed: 'rgb(192, 57, 43)'
   };
 
-  recordObj.hasMessage = !_.isNil(record.MESSAGE);
-%>
-<%-
-JSON.stringify(recordObj);
--%>
-<%
-});
-%>`, -1);
-  editor.focus();
+  /**
+   * @function handleEditorChange
+   * @description Render EJS template OR error stack to result DOM element.
+   */
+  const handleEditorChange = () => {
+    let result;
+    try {
+      result = ejs.render(editor.getValue());
+      resultEl.parentNode.style.background = colors.success;
+    } catch (e) {
+      result = e.stack;
+      resultEl.parentNode.style.background = colors.failed;
+    }
+
+    resultEl.textContent = result;
+  };
+
+  editor.on('change', handleEditorChange);
+  handleEditorChange();
+
+
+  /**
+   * @listens window.beforeunload
+   * @description Save the current editor text to load on next visit.
+   */
+  window.addEventListener('beforeunload', () => {
+    if (resultEl.parentNode.style.background === colors.success) {
+      window.localStorage.setItem('previous-session', editor.getValue());
+    }
+  });
 })();
